ae791d948a75b27858b90dd0e476a6b977bf668c
Shiv <raishiv@gmail.com>
2015-02-28 15:58:30 -0600

    2 new file added
    two new files added

---
+++ reverse_Cstring.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+using namespace std;
+char* reverse_word(char* word){
+	char* init = word;
+	char* last = word;
+	for(int i=0 ; word[i]; i++){
+		last++;
+	}
+	last--;
+	while(init < last){
+		char temp = (*init);
+		(*init) =  (*last);
+		(*last) = temp;
+		init++;
+		last--;
+	}
+	return word;
+}
+
+int main(){
+	char word[100];
+	cin>>word;
+	char* res = reverse_word(word);
+	cout<<res<<endl; 
+	return 0;
+}
---
+++ sentence_reverse.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+
+using namespace std;
+string reverse_word(string sen){
+	int len;
+	len = sen.length();
+	char* begin = &(sen[0]);
+	char* end = &(sen[len-1]);
+	while(begin < end){
+		char temp = *(begin);
+		(*begin) = (*end);
+		(*end) = temp;
+		begin++;
+		end--;
+	}
+	return sen;
+}
+
+int main(){
+	string sen;
+	getline(cin,sen);
+	sen = reverse_word(sen);
+	string buff;
+	stringstream ss(sen);
+	vector <string> tokens;
+	while(ss >> buff){
+		tokens.push_back(buff);
+	}
+	for(int k=0; k<tokens.size(); ++k ){
+		(tokens[k]) = reverse_word(tokens[k]);
+	}
+	string res = "";
+	for(int i=0; i<tokens.size(); ++i ){
+		res += tokens[i]+" ";
+	}
+	cout<<res<<endl;
+}
